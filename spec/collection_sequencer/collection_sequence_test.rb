7be3fbec4c572ecefc47d0c4b1d6afb36e5bd7e8
Jacek Szarski <jacek@applicake.com>
2010-04-13 11:28:25 +0200

    ids merging test

--- spec/collection_sequencer/collection_sequence_test.rb
+++ spec/collection_sequencer/collection_sequence_test.rb
@@ -10,4 +10,15 @@ describe "CollectionSequence test -> " do
     end
   end
 
+  describe "sorting" do
+    before :each do
+      @collection_sequence = CollectionSequence.create :ids => [1,2,3,4,5,6,7,8,9,0], :name => "my_sequece"
+    end
+
+    it "should merge ids on update" do
+      @collection_sequence.update_sequence! [4,3,9,8]
+      @collection_sequence.reload.ids.should == [1,2,4,3,5,6,7,9,8,0]
+    end
+  end
+
 end
